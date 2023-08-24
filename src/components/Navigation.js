c123d2f9b0c3952c8e7c0f2384d8a51fef56e3a7
spereira15 <105558705+spereira15@users.noreply.github.com>
2023-08-23 20:49:38 -0400

    update navigation paths

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,27 +6,27 @@ const Navigation = () => {
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          <NavLink to="/" exact className="nav-link" activeClassName="active">
+          <NavLink to="/react-portfolio-challenge" exact className="nav-link" activeClassName="active">
             Home
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/about" className="nav-link" activeClassName="active">
+          <NavLink to="/react-portfolio-challenge/about" className="nav-link" activeClassName="active">
             About
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/portfolio" className="nav-link" activeClassName="active">
+          <NavLink to="/react-portfolio-challenge/portfolio" className="nav-link" activeClassName="active">
             Portfolio
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/contact" className="nav-link" activeClassName="active">
+          <NavLink to="/react-portfolio-challenge/contact" className="nav-link" activeClassName="active">
             Contact
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/resume" className="nav-link" activeClassName="active">
+          <NavLink to="/react-portfolio-challenge/resume" className="nav-link" activeClassName="active">
             Resume
           </NavLink>
         </li>
